b9870f705905df18d450b1b678bfca4c185581c6
exister20 <yskmuhasebe@gmail.com>
2020-11-14 01:18:24 +0300

    Create logos.h

---
+++ logos.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void logo(){
+
+    printf("########           #####    #####   #####   \n");
+    printf("#                  #    #   #   #     #     \n");
+    printf("########   #####   #####    #####     #     \n");
+    printf("#                  #    #   #   #     #     \n");
+    printf("########           #    #   #   #     #     \n\n\n");
+
+}
